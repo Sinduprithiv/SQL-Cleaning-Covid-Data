c11551e298c300f7c374c2cc770adea1d109956e
Sinduprithiv <107149539+Sinduprithiv@users.noreply.github.com>
2022-06-15 14:57:15 -0400

    Update CovidDataClean.sql

--- CovidDataClean.sql
+++ CovidDataClean.sql
@@ -20,3 +20,32 @@ from c_case a1 join c_case a2
 on a1.row_date = a2.row_date-1
 where a1.location='Canada' and a2.location='Canada' and (Round((a1.PEOPLE_FULLY_VACCINATED/a1.population*100),4)) > (Round((a2.PEOPLE_FULLY_VACCINATED/a2.population*100),4))
 order by a1.row_date;
+
+//To view the join table data 
+select a1.row_date, a1.Total_Death, (Round((a1.total_death/a1.population*100),4)) as DEATH_PERCENTAGE, a2.*,
+(Round((a2.total_death/a2.population*100),4)) as DEATH_PERCENTAGE2
+from c_case a1 join c_case a2
+on a1.row_date = a2.row_date-1
+where a1.location='Canada' and a2.location='Canada'
+order by a1.row_date;
+
+//Query to Identify incorrect values in total_death column
+select a1.location, a1.row_date, a1.Total_Death, (Round((a1.total_death/a1.population*100),4)) as DEATH_PERCENTAGE 
+from c_case a1 join c_case a2
+on a1.row_date = a2.row_date-1
+where a1.location='Canada' and a2.location='Canada' and (Round((a1.total_death/a1.population*100),4)) > (Round((a2.total_death/a2.population*100),4)) 
+order by a1.row_date;
+
+//Select queries I expect to see null records
+  //check on median_age field
+  select count(location), location from (select distinct median_age, location from c_case group by location, median_Age) 
+  group by location having count(location)>1;
+
+  //check on population_density field
+  select count(location), location from (select distinct population_density, location from c_case group by location, population_density) 
+  group by location having count(location)>1;
+
+  select * from c_Case where row_date is null;
+
+  // Incorrect "Location" value- continent value where country value is expected
+  select distinct location from c_Case where continent is null;
